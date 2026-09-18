46579f81726d5aa097c280a215c184f0c3fce2d9
Pralhad <pralhaddeshpande117@gmail.com>
2026-09-18 15:39:01 +0530

    removed old method for choice , Added new methods : borrow , add books and tested it

--- src/Library_Management_System.java
+++ src/Library_Management_System.java
@@ -26,38 +26,65 @@ class StudentDetails extends Welcome{
 class Books extends StudentDetails{
     HashSet<String> book = new HashSet<>(Set.of("Java","C++","DSA","MY SQL"));
     Scanner sc = new Scanner(System.in);
-    String choice;
+    int noOfBooks;
+    String nameOfBook;
     void enterChoice(){
         System.out.println("Enter a/A for Checking Available books.");
         System.out.println("Enter b/B for borrowing the books.");
         System.out.println("Enter r/R for returning the book.");
         System.out.println("Enter p/P for adding books.");
         System.out.print("Enter the choice : ");
-        choice = sc.next();
-        if (choice=="a"||choice=="A"){
-            System.out.println("Available books : ");
-            availBooks();
-        }
-        else if(choice=="b"||choice=="B"){
-            borrowBooks();
-        }
+//        choice = sc.next();
+
     }
-    void availBooks(){
+    void checkAvailBooks(){
+        System.out.print("Available Books : ");
         System.out.println(book);
     }
-    void borrowBooks(){
-        System.out.println("Books");
+    void addBooks(){
+        System.out.print("How many books want to add : ");
+        noOfBooks=sc.nextInt();
+        for(int i=0;i<noOfBooks;i++){
+            System.out.print("Enter the name of the book : ");
+            nameOfBook=sc.next();
+            book.add(nameOfBook);
+        }
+        System.out.println("<<<Books added Successfully>>>");
+        checkAvailBooks();
     }
+    void borrowBook(){
+        System.out.println("Enter the name of the book : ");
+        nameOfBook=sc.next();
+//        if (book.contains(nameOfBook)){
+//            System.out.println("<<<Book Found>>>");
+//            book.remove(nameOfBook);
+//        }
+        
+        for(String b : book){
+            if(b.equalsIgnoreCase(nameOfBook)){
+                System.out.println("<<<Book Found>>>");
+                book.remove(nameOfBook);
+            }
+            return ;
+        }
+        System.out.println("Book Not Found");
+
+
+    }
+
 
 
 }
 
 public class Library_Management_System {
     static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Books sd = new Books();
         sd.welcome();
 //        sd.enterDetails();
-        sd.enterChoice();
-        sd.availBooks();
+        sd.checkAvailBooks();
+//        sd.addBooks();
+        sd.borrowBook();
+
     }
 }
